8ef6e7aaeacbb35984f8b0ebf5e6146dc4e2464a
rayan <rayanstive9@gmail.com>
2026-05-24 20:39:16 +0300

    feat(try): Update try

--- try.py
+++ try.py
@@ -1,4 +1,10 @@
-numbers = [8,4,5,1,3,9,7,6]
+nbs = [8,4,5,1,3,9,7,6]
 
-for nb in numbers:
-  print(nb)
+def try_number(numbers):
+  for i in range(len(numbers)-1):
+    if numbers[i+1] < numbers[i]:
+      numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
+  
+  return numbers  
+
+print(try_number(nbs))
