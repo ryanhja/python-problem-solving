fd07ea34683bcd07035cd3356546b094d5541984
rayan <rayanstive9@gmail.com>
2026-05-29 21:15:42 +0300

    feat(tri) update tri

--- try.py
+++ try.py
@@ -1,10 +1,11 @@
-nbs = [8,4,5,1,3,9,7,6]
+nbs = [8, 4, 5, 1, 3, 9, 7, 6]
 
-def try_number(numbers):
-  for i in range(len(numbers)-1):
-    if numbers[i+1] < numbers[i]:
-      numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
-  
-  return numbers  
+def tri_number(numbers):
+    for j in range(len(numbers)):
+        for i in range(len(numbers) - 1):
+            if numbers[i + 1] < numbers[i]:
+                numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
 
-print(try_number(nbs))
+    return numbers
+
+print(tri_number(nbs))
