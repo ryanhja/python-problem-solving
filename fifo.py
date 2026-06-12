46d0b7c1a767cda6598e413597b7047ac83ef70a
ryanhja <rayanstive9@gmail.com>
2026-06-12 22:24:05 +0300

    feat(algo): Add fifo

--- fifo.py
+++ fifo.py
@@ -5,6 +5,7 @@ students = deque()
 students.append("Student One")
 students.append("Student Two")
 students.append("Student Three")
+students.append("Student Four")
 
 while students:
     print("In progress: ", students.popleft())
